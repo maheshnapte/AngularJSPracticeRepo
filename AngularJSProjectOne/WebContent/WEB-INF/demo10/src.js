ad710d3c5678f7eceeef5d6136fa0e9fdfb79d73
Mahesh Napte <memaheshnapte@gmail.com>
2018-12-19 10:04:38 +0530

    bulk commit :).

---
+++ AngularJSProjectOne/WebContent/WEB-INF/demo10/src.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp', []);
+
+app.controller('namesCtrl', function($scope) {
+	$scope.msg= "Hi Mahesh ...";
+});
+
+app.controller('namesCtrl1', function($scope) {
+	
+});
+
+app.controller('namesCtrl2', function($scope) {
+	
+});
---
+++ AngularJSProjectOne/WebContent/WEB-INF/demo2/scr1.js
@@ -0,0 +1,31 @@
+//n the same page. Many thanks for all the inputs.
+
+var shoppingCartModule = angular.module("shoppingCart", [])
+shoppingCartModule.controller("ShoppingCartController",
+  function($scope) {
+    $scope.items = [{
+      product_name: "Product 1",
+      price: 50
+    }, {
+      product_name: "Product 2",
+      price: 20
+    }, {
+      product_name: "Product 3",
+      price: 180
+    }];
+    $scope.remove = function(index) {
+      $scope.items.splice(index, 1);
+    }
+  }
+);
+var namesModule = angular.module("namesList", [])
+namesModule.controller("NamesController",
+  function($scope) {
+    $scope.names = [{
+      username: "Nitin"
+    }, {
+      username: "Mukesh"
+    }];
+  }
+);
+angular.bootstrap(document.getElementById("App2"), ['namesList']);
